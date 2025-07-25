3906398066580005061914405ffa7a1f4e7cf4f9
MaheenHabib <maheenhabib05@gmail.com>
2025-07-25 23:51:57 +0500

    done

---
+++ age&name.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int age;
+string name;
+    cout<<"enter your age"<<endl;
+    cin>>age;
+    cout<<"enter your name"<<endl;
+    cin>>name; 
+    cout<<"this is my name"<<name;
+    return 0;
+}
