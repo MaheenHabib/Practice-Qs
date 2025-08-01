5886e685b29968d6550b7a1ea7c47caaa9dd4ce8
MaheenHabib <maheenhabib05@gmail.com>
2025-08-01 21:21:02 +0500

    done

---
+++ Logical Error.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int x = 5;
+    int y = 7;
+    int average = x*y;  /* Fromula to find average: x/y */
+    
+    cout << "Average:" << average;
+    
+    return 0;
+}
