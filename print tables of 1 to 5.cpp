7b1e9b6eb95dbf26f4cd3da85422ea29b6045a4a
MaheenHabib <maheenhabib05@gmail.com>
2025-09-04 05:12:50 -0700

    done

---
+++ print tables of 1 to 5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+ int i, j;
+ 
+ for(i=1; i<=5; i++)
+ {
+  for(j=1; j<=10; j++)
+  {
+   cout <<i<<" * "<<j<<" = "<<i*j<< endl;
+  }
+  cout << endl;
+ }
+
+
+
+   return 0;
+}
